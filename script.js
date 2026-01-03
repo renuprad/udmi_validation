5ad4017c296f1f824a67ac6cd294a93eb851b9ee
Renuka Prasad Nagaraj <renukanagaraj@google.com>
2026-01-03 07:18:31 +0000

    added .js file

--- script.js
+++ script.js
@@ -9,38 +9,38 @@
 
 console.log('There correct devices list is 2nd time:', myGlobalValue)
 
-const container = document.getElementById('data-container');
-console.log('The first log', container);
+//const container = document.getElementById('data-container');
+//console.log('The first log', container);
 
-const dataArray = container.getAttribute('data-array-values');
-console.log('The second log', dataArray);
+//const dataArray = container.getAttribute('data-array-values');
+//console.log('The second log', dataArray);
 
-if (dataArray){
-	const myArray = JSON.parse(dataArray);
-	console.log("External Script: Accessing array via data attribute:", myArray);
-} else {
-	console.log("External Script: Data attribute not found.");
-}
+//if (dataArray){
+//	const myArray = JSON.parse(dataArray);
+//	console.log("External Script: Accessing array via data attribute:", myArray);
+//} else {
+//	console.log("External Script: Data attribute not found.");
+//}
 	
 
-createTable(dataArray);
+//createTable(dataArray);
 
-const leng = dataArray.length;
-console.log(leng);
+//const leng = dataArray.length;
+//console.log(leng);
 
-function createTable(dataArray1) {
-	const table = document.getElementById('dataTable');
- 	
- 	const headerRow = table.insertRow();
- 	const headerCell = headerRow.insertCell();
- 	headerCell.textContent = "Correct Devices"; // Give a meaningful header name
- 	headerCell.style.fontWeight = "bold";
-	
-	dataArray.forEach(item => {
-		const row = table.insertRow(); // Insert a new row at the end of the table
-		const cell = row.insertCell(); // Insert a cell into the new row
-		cell.textContent = item; // Set the cell's text content to the array item
-	});
-}
+//function createTable(dataArray1) {
+//	const table = document.getElementById('dataTable');
+// 	
+// 	const headerRow = table.insertRow();
+// 	const headerCell = headerRow.insertCell();
+// 	headerCell.textContent = "Correct Devices"; // Give a meaningful header name
+// 	headerCell.style.fontWeight = "bold";
+//
+//	dataArray.forEach(item => {
+//		const row = table.insertRow(); // Insert a new row at the end of the table
+//		const cell = row.insertCell(); // Insert a cell into the new row
+//		cell.textContent = item; // Set the cell's text content to the array item
+//	});
+// }
 
 
